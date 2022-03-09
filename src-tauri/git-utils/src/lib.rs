c99f0394c418b701008bbc3e16a2d3d6dc445014
Seokju Na <seokju.me@toss.im>
2022-03-09 20:59:26 +0900

    add `GitUtils::read_index_entries`

--- src-tauri/git-utils/src/lib.rs
+++ src-tauri/git-utils/src/lib.rs
@@ -1,4 +1,4 @@
-use git2::{Commit, Error as Git2Error, Oid, Repository};
+use git2::{Commit, Error as Git2Error, IndexEntry, Oid, Repository};
 
 use constants::EOL;
 
@@ -51,6 +51,13 @@ impl GitUtils {
     Ok(commits)
   }
 
+  pub fn read_index_entries(repo: &Repository) -> Result<Vec<IndexEntry>, Git2Error> {
+    let index = repo.index()?;
+    let entries: Vec<_> = index.iter().collect();
+
+    Ok(entries)
+  }
+
   pub fn parse_commit_message(message: &str) -> (String, String) {
     let lines: Vec<_> = message.split(EOL).collect();
     let subject = lines.get(0).unwrap_or(&"");
@@ -70,6 +77,8 @@ impl GitUtils {
 
 #[cfg(test)]
 mod git_utils_tests {
+  use std::path::Path;
+  use std::str::from_utf8;
   use testing::git::FixtureRepository;
 
   use super::*;
@@ -99,6 +108,42 @@ mod git_utils_tests {
 
     let commits = GitUtils::read_commits_from_head(&repo).unwrap();
     assert_eq!(commits.len(), 5);
+  }
+
+  #[test]
+  fn should_read_index_entries() {
+    let fixture = FixtureRepository::setup_with_script(
+      r#"
+    echo "a" > a.txt
+    echo "b" > b.txt
+    mkdir -p 1/
+    echo "1/c" > 1/c.txt
+    "#,
+    );
+    let repo = Repository::open(&fixture.path).unwrap();
+
+    let entries = GitUtils::read_index_entries(&repo).unwrap();
+    assert_eq!(entries.len(), 0);
+
+    // 1. commit "a.txt", "b.txt"
+    let mut index = repo.index().unwrap();
+    index.add_path(Path::new("a.txt")).unwrap();
+    index.add_path(Path::new("b.txt")).unwrap();
+    GitUtils::commit_on_head(&repo, "1").unwrap();
+
+    let entries = GitUtils::read_index_entries(&repo).unwrap();
+    assert_eq!(entries.len(), 2);
+    assert_eq!(from_utf8(&entries.get(0).unwrap().path).unwrap(), "a.txt");
+    assert_eq!(from_utf8(&entries.get(1).unwrap().path).unwrap(), "b.txt");
+
+    // 2. commit "1/c.txt"
+    let mut index = repo.index().unwrap();
+    index.add_path(Path::new("1/c.txt")).unwrap();
+    GitUtils::commit_on_head(&repo, "2").unwrap();
+
+    let entries = GitUtils::read_index_entries(&repo).unwrap();
+    assert_eq!(entries.len(), 3);
+    assert_eq!(from_utf8(&entries.get(0).unwrap().path).unwrap(), "1/c.txt");
   }
 
   #[test]
