7c50fca718badcfa89f33b6de6807fee672a28a1
Seokju Na <seokju.me@toss.im>
2022-03-09 21:00:12 +0900

    manage `Workspace` as tauri state

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,23 +3,30 @@
   windows_subsystem = "windows"
 )]
 
+use tauri::Manager;
+
+use crate::commands::read_git_index_entries;
+use crate::workspace::Workspace;
+
 mod app_error;
+mod commands;
 mod git_eventstore;
+mod workspace;
 
 fn main() {
   tauri::Builder::default()
     .setup(|app| {
-      use tauri::GlobalShortcutManager;
+      let app_dir = app
+        .path_resolver()
+        .app_dir()
+        .expect("Cannot resolve app dir");
 
-      let mut shortcut = app.global_shortcut_manager();
-      shortcut
-        .register("Cmd+G", || {
-          println!("hello");
-        })
-        .unwrap();
+      let workspace = Workspace::initialize(app_dir).expect("fail to initialize workspace");
+      app.manage(workspace);
 
       Ok(())
     })
+    .invoke_handler(tauri::generate_handler![read_git_index_entries])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
