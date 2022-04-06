684d1d96790ac0a74de3b3e04b707fa15f0a2c27
Seokju Na <seokju.me@toss.im>
2022-04-07 01:00:44 +0900

    remove `event-sourcing`

--- src-tauri/event-sourcing/src/eventstore.rs
+++
@@ -1,8 +0,0 @@
-use crate::event::Event;
-use crate::eventstore_error::EventstoreError;
-use crate::recorded_event::RecordedEvent;
-
-pub trait Eventstore {
-  fn append(&mut self, event: Event) -> Result<RecordedEvent, EventstoreError>;
-  fn read(&mut self) -> Result<Vec<RecordedEvent>, EventstoreError>;
-}
--- src-tauri/event-sourcing/src/recorded_event.rs
+++
@@ -1,8 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct RecordedEvent {
-  pub id: String,
-  pub name: String,
-  pub data: String,
-}
