434cb3b7704edf499c95a44d4872ce92c61ae609
Seokju Na <seokju.me@toss.im>
2022-04-17 23:09:51 +0900

    refactor es system
    referenced interfaces: [eventually-rs](https://github.com/get-eventually/eventually-rs)

--- src-tauri/core/src/event.rs
+++ src-tauri/core/src/event.rs
@@ -1,31 +1,17 @@
-use serde::de::DeserializeOwned;
 use serde::{Deserialize, Serialize};
-use serde_json::Value;
 
-use crate::aggregate::AggregateState;
+use crate::Version;
 
-#[derive(thiserror::Error, Debug)]
-pub enum EventParseError {
-  #[error("Fail")]
-  Fail,
-  #[error("No Matches")]
-  NoMatches,
+pub trait Event: Send + Sync {
+  fn name(&self) -> &'static str;
 }
 
-pub trait Event<S>: Serialize + DeserializeOwned
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+pub struct PersistedEvent<T>
 where
-  S: AggregateState,
+  T: Event,
 {
-  fn from_event_data(event_data: EventData) -> Result<Self, EventParseError>;
-  fn to_event_data(self) -> EventData;
-  fn handle(&self, state: &mut S) -> ();
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
-pub struct EventData {
-  pub name: String,
   pub aggregate_id: String,
-  pub aggregate_version: i64,
-  pub timestamp: i64,
-  pub payload: Value,
+  pub version: Version,
+  pub event: T,
 }
