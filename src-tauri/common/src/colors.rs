09593edaf2fa02ab4eb2e1baa3694a5bfa80183a
Seokju Na <seokju.me@toss.im>
2022-04-17 23:09:00 +0900

    add `PartialEq`

--- src-tauri/common/src/colors.rs
+++ src-tauri/common/src/colors.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
 pub struct RGB {
   pub r: u8,
   pub g: u8,
