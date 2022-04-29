d9993ed570f6f3c72713ed4550483b0dec02f689
Seokju Na <seokju.me@toss.im>
2022-04-29 20:53:40 +0900

    remove unused code

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,29 +3,8 @@
   windows_subsystem = "windows"
 )]
 
-use tauri::Manager;
-
-use crate::commands::read_git_index_entries;
-use crate::workspace::Workspace;
-
-mod app_error;
-mod commands;
-mod workspace;
-
 fn main() {
   tauri::Builder::default()
-    .setup(|app| {
-      let app_dir = app
-        .path_resolver()
-        .app_dir()
-        .expect("Cannot resolve app dir");
-
-      let workspace = Workspace::initialize(app_dir).expect("fail to initialize workspace");
-      app.manage(workspace);
-
-      Ok(())
-    })
-    .invoke_handler(tauri::generate_handler![read_git_index_entries])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
