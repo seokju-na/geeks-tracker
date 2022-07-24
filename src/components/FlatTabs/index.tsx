b068eb7720dcf1dc982beccb4c84f9aa8fadceb1
Seokju Na <seokju.me@toss.im>
2022-07-24 20:01:28 +0900

    add FlatTabs component

---
+++ src/components/FlatTabs/index.tsx
@@ -0,0 +1,39 @@
+import * as Primitives from '@radix-ui/react-tabs';
+import { styled } from '../../styles';
+
+const Root = styled(Primitives.Root, {});
+
+const List = styled(Primitives.List, {
+  display: 'flex',
+});
+
+const Trigger = styled(Primitives.Trigger, {
+  all: 'unset',
+  backgroundColor: '$background',
+  height: 28,
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  padding: '0 $xl',
+  fontSize: '$md',
+  color: '$text',
+  userSelect: 'none',
+  cursor: 'pointer',
+  '& + &': {
+    borderLeft: '1px solid $divider',
+  },
+  '&[aria-selected="true"]': {
+    backgroundColor: '$backgroundHighlighted',
+  },
+});
+
+const Content = styled(Primitives.Content, {
+  outline: 'none',
+});
+
+export const FlatTabs = {
+  Root,
+  List,
+  Trigger,
+  Content,
+};
