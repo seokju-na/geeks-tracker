ad5912ece567d1386ec9bc06d7628a41a1351781
Seokju Na <seokju.me@toss.im>
2022-07-30 19:30:56 +0900

    editor styling

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -1,4 +1,5 @@
 import { useEditor } from '../../hooks/useEditor';
+import { styled } from '../../styles';
 
 interface Props {
   initialDoc?: string;
@@ -7,5 +8,34 @@ interface Props {
 export function Editor(props: Props) {
   const [elemRef] = useEditor<HTMLDivElement>(props);
 
-  return <div ref={elemRef} />;
+  return <Wrapper ref={elemRef} />;
 }
+
+const Wrapper = styled('div', {
+  outline: 'none',
+  '.cm-editor': {
+    color: '$text',
+    backgroundColor: '$background',
+    fontFamily:
+      'ui-sans-serif, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Inter", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Microsoft YaHei Light", sans-serif',
+    fontWeight: 500,
+  },
+  '.cm-content': {
+    caretColor: '$text',
+  },
+  '.cm-focused .cm-selectionBackground, ::selection': {
+    backgroundColor: '$blue100',
+  },
+  '.cm-gutters': {
+    backgroundColor: '$background',
+    borderRight: '1px solid $divider',
+    color: '$text',
+  },
+  '.cm-activeLine': {
+    backgroundColor: '$backgroundHighlighted',
+    fontWeight: 400,
+  },
+  '.cm-activeLineGutter': {
+    backgroundColor: '$backgroundHighlighted',
+  },
+});
