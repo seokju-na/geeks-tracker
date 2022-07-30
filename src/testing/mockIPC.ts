ea444afb9e289d778399097fc8ff4abef3be6450
Seokju Na <seokju.me@toss.im>
2022-07-30 14:34:53 +0900

    mock tauri ipc, event

---
+++ src/testing/mockIPC.ts
@@ -0,0 +1,40 @@
+import type { TauriCommand } from '@tauri-apps/api/helpers/tauri';
+import { mockIPC as mockTauriIPC } from '@tauri-apps/api/mocks';
+
+type Handler = (args: Record<string, unknown>) => any;
+const tauriEventHandlers = new Map<string, Handler>(); // handlers for tauri event
+const handlers = new Map<string, Handler>();
+
+beforeEach(() => {
+  mockTauriIPC((command, args) => {
+    if (command === 'tauri') {
+      const module = (args as TauriCommand).__tauriModule;
+      switch (module) {
+        case 'Event': {
+          const handler = tauriEventHandlers.get((args as any).message.event);
+          return handler?.(args);
+        }
+        default:
+          throw new Error('TODO');
+      }
+    }
+
+    return handlers.get(command)?.(args);
+  });
+});
+
+afterEach(() => {
+  tauriEventHandlers.clear();
+  handlers.clear();
+});
+
+export function mockIPC(command: string, handler: Handler) {
+  if (command === 'tauri') {
+    throw new Error(`Cannot mock "tauri" command. Use other mock instead.`);
+  }
+  handlers.set(command, handler);
+}
+
+export function mockTauriEvent(event: string, handler: Handler) {
+  tauriEventHandlers.set(event, handler);
+}
