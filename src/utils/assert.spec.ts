56c4dab1a45af1d7b22ed5a87db59f9ec6929ca3
Seokju Na <seokju.me@gmail.com>
2022-07-31 20:56:27 +0900

    vitest migrate

--- src/utils/assert.spec.ts
+++ src/utils/assert.spec.ts
@@ -1,21 +1,20 @@
+import { it } from 'vitest';
 import { assert, AssertionError } from './assert';
 
-describe('assert', () => {
-  it('should throw "AssertionError" when condition falsy', () => {
-    const a: number = 10;
+it('should throw "AssertionError" when condition falsy', () => {
+  const a: number = 10;
 
-    expect(() => assert(a === 20)).toThrowError(new AssertionError());
-  });
+  expect(() => assert(a === 20)).toThrowError(new AssertionError());
+});
 
-  it('should throw custom error', () => {
-    const a: number = 15;
+it('should throw custom error', () => {
+  const a: number = 15;
 
-    class MyCustomError extends Error {
-      constructor() {
-        super('error!');
-      }
+  class MyCustomError extends Error {
+    constructor() {
+      super('error!');
     }
+  }
 
-    expect(() => assert(a === 20, new MyCustomError())).toThrowError(new MyCustomError());
-  });
+  expect(() => assert(a === 20, new MyCustomError())).toThrowError(new MyCustomError());
 });
