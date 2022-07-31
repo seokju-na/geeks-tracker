30a4412b0d147e91407bf440c20969dc19e4dfd9
Seokju Na <seokju.me@gmail.com>
2022-07-31 20:58:01 +0900

    add CategoryCommand type

---
+++ src/types/CategoryCommand.ts
@@ -0,0 +1,30 @@
+export type CategoryCommandName =
+  | 'CategoryCommand.Create'
+  | 'CategoryCommand.UpdateTitle'
+  | 'CategoryCommand.UpdateTemplate';
+
+interface Base {
+  name: CategoryCommandName;
+}
+
+interface CreateCommand extends Base {
+  name: 'CategoryCommand.Create';
+  id: string;
+  title: string;
+  template: string;
+}
+
+interface UpdateTitleCommand extends Base {
+  name: 'CategoryCommand.UpdateTitle';
+  id: string;
+  title: string;
+}
+
+interface UpdateTemplateCommand extends Base {
+  name: 'CategoryCommand.UpdateTemplate';
+  id: string;
+  template: string;
+}
+
+export type CategoryCommand = CreateCommand | UpdateTitleCommand | UpdateTemplateCommand;
+export type CategoryCommandOf<T extends CategoryCommandName> = Extract<CategoryCommand, { name: T }>;
