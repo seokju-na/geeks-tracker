cf302b3d142cc6b2d760b8a279d77f53e384f83f
Seokju Na <seokju.me@gmail.com>
2022-08-04 18:25:23 +0900

    add DummyCategory

---
+++ src/testing/DummyCategory.ts
@@ -0,0 +1,10 @@
+import { each, Sync } from 'factory.ts';
+import { Category } from '../types/Category';
+
+export const DummyCategory = Sync.makeFactory<Category>({
+  id: each(x => `category-${x}`),
+  title: each(x => `category-title-${x}`),
+  template: each(x => `category-template-${x}`),
+  createdAt: Date.now(),
+  updatedAt: Date.now(),
+});
