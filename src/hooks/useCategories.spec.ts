53fcc5b60ea62790c85b074b7a8581464fa4d0a4
Seokju Na <seokju.me@gmail.com>
2022-08-04 22:03:15 +0900

    add selected category state

--- src/hooks/useCategories.spec.ts
+++ src/hooks/useCategories.spec.ts
@@ -1,7 +1,8 @@
-import { waitFor } from '@testing-library/react';
+import { act, waitFor } from '@testing-library/react';
 import { expect, it, vitest } from 'vitest';
 import { DummyCategory } from '../testing/DummyCategory';
 import { mockIPC } from '../testing/mockIPC';
+import { mockStore } from '../testing/mockStore';
 import { renderHookWithTestBed } from '../testing/renderWithTestBed';
 import { useCategories } from './useCategories';
 
@@ -34,5 +35,42 @@ it('add category', async () => {
       title: 'My New Category',
       template: 'this is template',
     }),
+  });
+});
+
+it('get last selected id from ".history" store', async () => {
+  const categories = DummyCategory.buildList(5);
+  mockStore('.history').set('lastSelectedCategoryId', categories[2]!.id);
+
+  const { result } = renderHookWithTestBed(() => useCategories());
+  await waitFor(() => {
+    expect(result.current.selectedCategory).toEqual(categories[2]);
+  });
+});
+
+it('get select id from first category if last selected id is not exists', async () => {
+  const categories = DummyCategory.buildList(5);
+  mockIPC('list_categories', () => categories);
+
+  const { result } = renderHookWithTestBed(() => useCategories());
+  await waitFor(() => {
+    expect(result.current.selectedCategory).toEqual(categories[0]);
+  });
+});
+
+it('select category id', async () => {
+  const categories = DummyCategory.buildList(5);
+  mockIPC('list_categories', () => categories);
+  mockStore('.history').set('lastSelectedCategoryId', categories[0]!.id);
+
+  const { result } = renderHookWithTestBed(() => useCategories());
+  await waitFor(() => {
+    expect(result.current.selectedCategory).toEqual(categories[0]);
+  });
+  await act(async () => {
+    await result.current.selectCategory(categories[2]!.id);
+  });
+  await waitFor(() => {
+    expect(result.current.selectedCategory).toEqual(categories[2]);
   });
 });
--- src/hooks/useCategories.ts
+++ src/hooks/useCategories.ts
@@ -4,13 +4,9 @@ import { queryClient } from '../queryClient';
 import { executeCategoryCommand } from '../remotes/executeCategoryCommand';
 import { listCategories } from '../remotes/listCategories';
 import { uid } from '../utils/uid';
+import { useStore } from './useStore';
 
 const queryKey = ['categories'];
-
-interface AddCategory {
-  title: string;
-  template: string;
-}
 
 function invalidateQueries() {
   return queryClient.invalidateQueries(queryKey);
@@ -19,19 +15,34 @@ function invalidateQueries() {
 export function useCategories() {
   const { data: categories = [] } = useQuery(queryKey, () => listCategories());
 
-  const addCategory = useCallback(async ({ title, template }: AddCategory) => {
+  const addCategory = useCallback(async (payload: { title: string; template: string }) => {
     await executeCategoryCommand({
       name: 'CategoryCommand.Create',
       id: uid(),
-      title,
-      template,
+      ...payload,
     });
     await invalidateQueries();
   }, []);
 
+  const { value: lastSelectedCategoryId, setValue: setSelectedCategoryId } = useStore<string>(
+    '.history',
+    'lastSelectedCategoryId'
+  );
+  const selectedCategoryId = lastSelectedCategoryId ?? categories[0]?.id;
+
+  const selectCategory = useCallback(
+    (categoryId: string) => {
+      return setSelectedCategoryId(categoryId);
+    },
+    [setSelectedCategoryId]
+  );
+
   return {
     categories,
+    selectedCategoryId,
+    selectedCategory: categories.find(x => x.id === selectedCategoryId),
     addCategory,
+    selectCategory,
   } as const;
 }
 
