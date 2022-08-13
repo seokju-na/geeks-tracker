5beebcba517628a8a00b05c9c555d369cd3ae9b0
Seokju Na <seokju.me@gmail.com>
2022-08-13 16:18:29 +0900

    impl `get_note` query handler

--- src-tauri/src/application/query_handler.rs
+++ src-tauri/src/application/query_handler.rs
@@ -1,12 +1,17 @@
 use crate::application::Application;
-use crate::domain::Category;
+use crate::domain::{Category, Note};
 
 pub trait QueryHandler {
   fn list_categories(&self) -> Vec<Category>;
+  fn get_note(&self, id: String) -> Option<Note>;
 }
 
 impl QueryHandler for Application {
   fn list_categories(&self) -> Vec<Category> {
     self.categories.states.values().cloned().collect()
+  }
+
+  fn get_note(&self, id: String) -> Option<Note> {
+    self.notes.get_state(id).cloned()
   }
 }
