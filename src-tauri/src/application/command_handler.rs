1ecba5efef5d5c8d6fc2962b5a7887497d5211f8
Seokju Na <seokju.me@gmail.com>
2022-08-13 15:28:57 +0900

    add note command handler

--- src-tauri/src/application/command_handler.rs
+++ src-tauri/src/application/command_handler.rs
@@ -3,7 +3,7 @@ use geeks_event_sourcing::{Command, Eventstore};
 use geeks_event_sourcing_git::GitEventstore;
 
 use crate::application::{Application, ApplicationError};
-use crate::domain::CategoryCommand;
+use crate::domain::{CategoryCommand, NoteCommand};
 
 #[async_trait]
 pub trait CommandHandler<T>
@@ -17,6 +17,17 @@ where
 impl CommandHandler<CategoryCommand> for Application {
   async fn handle_command(&mut self, command: CategoryCommand) -> Result<(), ApplicationError> {
     let event = self.categories.execute_command(command)?;
+    let eventstore = GitEventstore::new(&self.workspace_dir);
+    eventstore.append(vec![event]).await?;
+
+    Ok(())
+  }
+}
+
+#[async_trait]
+impl CommandHandler<NoteCommand> for Application {
+  async fn handle_command(&mut self, command: NoteCommand) -> Result<(), ApplicationError> {
+    let event = self.notes.execute_command(command)?;
     let eventstore = GitEventstore::new(&self.workspace_dir);
     eventstore.append(vec![event]).await?;
 
