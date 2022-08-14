8e621a1d744fadfb309d6830e0247908923e2288
Seokju Na <seokju.me@gmail.com>
2022-08-14 18:17:57 +0900

    fix

--- src/testing/DummyCategory.ts
+++ src/testing/DummyCategory.ts
@@ -5,6 +5,7 @@ export const DummyCategory = Sync.makeFactory<Category>({
   id: each(x => `category-${x}`),
   title: each(x => `category-title-${x}`),
   template: each(x => `category-template-${x}`),
+  order: 0,
   createdAt: Date.now(),
   updatedAt: Date.now(),
 });
