7b910fc1441458ad25c7594b261de4b93cdd24ee
Seokju Na <seokju.me@gmail.com>
2022-08-14 23:45:07 +0900

    use memory history

--- src/location.ts
+++ src/location.ts
@@ -1,3 +1,6 @@
-import { ReactLocation } from '@tanstack/react-location';
+import { createMemoryHistory, ReactLocation } from '@tanstack/react-location';
 
-export const location = new ReactLocation();
+export const location = new ReactLocation({
+  // NOTE: use memory history because we uses file protocol for serving webview.
+  history: createMemoryHistory(),
+});
