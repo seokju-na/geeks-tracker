c605d9dd64875c2f81f0515e1a1b6d9d1451e01c
Seokju Na <seokju.me@gmail.com>
2022-08-15 16:49:45 +0900

    add tray menu

--- src-tauri/src/tray.rs
+++ src-tauri/src/tray.rs
@@ -1,13 +1,52 @@
-use tauri::{AppHandle, Runtime, SystemTray, SystemTrayEvent};
+use tauri::{
+  AppHandle, CustomMenuItem, Runtime, SystemTray, SystemTrayEvent, SystemTrayMenu,
+  SystemTrayMenuItem,
+};
 
+use crate::constants::{ACCELERATOR_OPEN, ACCELERATOR_PREFERENCES};
+use crate::os_type::os_type;
 use crate::windows::{AppExtra, WindowExtra};
 
 pub fn tray() -> SystemTray {
-  SystemTray::new()
+  SystemTray::new().with_menu(menu())
 }
 
 pub fn handle_tray<R: Runtime>(app: &AppHandle<R>, event: SystemTrayEvent) {
-  if let SystemTrayEvent::LeftClick { .. } = event {
-    app.get_main_window().toggle().unwrap();
+  if let SystemTrayEvent::MenuItemClick { id, .. } = event {
+    match id.as_str() {
+      "open" => {
+        app.get_main_window().show().unwrap();
+      }
+      "preferences" => {
+        let win = app.get_main_window();
+        win.navigate("/preferences").unwrap();
+        win.show().unwrap();
+      }
+      "quit" => {
+        std::process::exit(0);
+      }
+      _ => {}
+    }
   }
+}
+
+fn menu() -> SystemTrayMenu {
+  let open =
+    CustomMenuItem::new("open".to_string(), "Open Geek's Tracker").accelerator(ACCELERATOR_OPEN);
+  let preferences = CustomMenuItem::new(
+    "preferences".to_string(),
+    match os_type() {
+      "macos" => "Preferences...",
+      _ => "Preferences",
+    },
+  )
+  .accelerator(ACCELERATOR_PREFERENCES);
+  let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+
+  SystemTrayMenu::new()
+    .add_item(open)
+    .add_native_item(SystemTrayMenuItem::Separator)
+    .add_item(preferences)
+    .add_native_item(SystemTrayMenuItem::Separator)
+    .add_item(quit)
 }
